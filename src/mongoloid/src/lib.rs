6772cdee319481d698d7bfbc3c940b50c389825c
An Camchéachta <ancamcheachta@protonmail.com>
2017-10-21 18:16:12 +0000

    Support for mongodb insert_many, changes to CreateDatabase trait in mongoloid

--- src/mongoloid/src/lib.rs
+++ src/mongoloid/src/lib.rs
@@ -6,6 +6,7 @@ extern crate mongodb;
 extern crate serde;
 extern crate serde_json;
 
+use bson::ordered::OrderedDocument;
 use mongodb::{Client, ClientInner, ThreadedClient};
 use mongodb::db::ThreadedDatabase;
 use std::env::current_dir;
@@ -22,7 +23,7 @@ const DAIL: &'static str = "dail";
 const WESTMINSTER: &'static str = "westminster";
 const CONSTITUENCIES: &'static str = "constituencies";
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug, Default, Clone)]
 #[serde(default)]
 struct Area {
     area_type: String,
@@ -39,7 +40,7 @@ struct Area {
     year: i16,
 }
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug, Default, Clone)]
 #[serde(default)]
 struct Candidate {
     counts: Vec<i32>,
@@ -92,13 +93,13 @@ trait CreateDatabase {
 	// Validate constituencies directory exists, then read and walk it
 	fn create_constituencies(&self);
 	// Validate constituencies contents are directories, then read and walk them
-	fn walk_constituency_subdirs(&self, dir: &PathBuf);
+	fn walk_constituency_subdirs(&self, dir: &PathBuf) -> Option<Vec<Area>>;
 	// Validate subdirectory election json files exist, then read and walk them
-	fn walk_json_files(&self, subdir: &PathBuf);
+	fn walk_json_files(&self, subdir: &PathBuf) -> Option<Vec<Area>>;
 	// Serialise election json with serde
-	fn load_json(&self, jsonfile: &PathBuf);
+	fn load_json(&self, jsonfile: &PathBuf) -> Option<Area>;
 	// Serialise doc as BSON and make mongodb request
-	fn create_document<'a, T>(&self, doc: &'a T) where T: serde::Serialize;
+	fn create_document<'a,T>(&self, areas: &'a Vec<T>) where T: serde::Serialize;
 }
 
 impl CreateDatabase for ElectionDatabase {
@@ -106,67 +107,98 @@ impl CreateDatabase for ElectionDatabase {
         let constituencies_dir = current_dir().unwrap().join(CONSTITUENCIES);
         
         match constituencies_dir.exists() {
-            true    => self.walk_constituency_subdirs(&constituencies_dir),
+            true    => match self.walk_constituency_subdirs(&constituencies_dir) {
+                    Some(ref a) => self.create_document(a),
+                    None        => {
+                        eprintln!("{}","[mongoloid] No constituencies to create.");
+                        process::exit(1);
+                    }
+                },
             false   => {
                     eprintln!("[mongoloid] Current working directory is missing '{}' sub-directory. Exiting.", CONSTITUENCIES);
                     process::exit(1);
                 },
         }
     }
-    fn walk_constituency_subdirs(&self, constituencies_dir: &PathBuf) {
+    fn walk_constituency_subdirs(&self, constituencies_dir: &PathBuf) -> Option<Vec<Area>> {
         let dir = fs::read_dir(constituencies_dir).unwrap();
-        dir.filter_map(|entry| {
+        let areas: Vec<Area> = dir.filter_map(|entry| {
             let entry = entry.unwrap();
             let subdir: PathBuf = entry.path();
             
             match subdir.is_dir() {
-                true    => Some(self.walk_json_files(&subdir)),
+                true    => self.walk_json_files(&subdir),
                 false   => None
             }
 
-        }).collect::<Vec<_>>();
+        }).collect::<Vec<Vec<Area>>>()
+        .iter()
+        .flat_map(|a| a.iter().map(Clone::clone))
+        .collect();
+        
+        match areas.is_empty() {
+            true    => None,
+            false   => Some(areas)
+        }
     }
-    fn walk_json_files(&self, subdir: &PathBuf) {
+    fn walk_json_files(&self, subdir: &PathBuf) -> Option<Vec<Area>> {
+        let mut areas: Vec<Area> = Vec::new();
         match fs::read_dir(subdir) {
-            Ok(dir) => {
-                    dir.filter_map(|entry| {
-                        let entry = entry.unwrap();
-                        let jsonfile: PathBuf = entry.path();
-                        
-                        match jsonfile.is_file() {
-                            true    => Some(self.load_json(&jsonfile)),
-                            false   => None
-                        }
-                    }).collect::<Vec<_>>();
-                },
             Err(why)  => panic!("[mongoloid] Couldn't read directory. {}", why),
+            Ok(dir) => {
+                let a = dir.filter_map(|entry| {
+                    let entry = entry.unwrap();
+                    let jsonfile: PathBuf = entry.path();
+                    
+                    match jsonfile.is_file() {
+                        true    => self.load_json(&jsonfile),
+                        false   => None
+                    }
+                }).collect::<Vec<Area>>();
+                if !a.is_empty() {
+                    areas.extend(a);
+                }
+            },
         };
+        match areas.is_empty() {
+            true    => None,
+            false   => Some(areas)
+        }
     }
-    fn load_json(&self, jsonfile: &PathBuf) {
+    fn load_json(&self, jsonfile: &PathBuf) -> Option<Area>{
         println!("[mongoloid] Processing {:?}", jsonfile);
         match File::open(&jsonfile) {
             Err(why) => panic!("Couldn't open file: {}", why),
             Ok(file) => {
-                let constituency: Area = match serde_json::from_reader(file) {
+                let area: Option<Area> = match serde_json::from_reader(file) {
+                    Ok(a) => Some(a),
                     Err(why) => panic!("[mongoloid] Couldn't deserialise: {}", why),
-                    Ok(constituency) => constituency,
                 };
-                self.create_document(&constituency);
+                return area;
             },
-        };
+        }
     }
-    fn create_document<'a, T>(&self, doc: &'a T) where T: serde::Serialize {
-        match bson::to_bson(doc) {
-            Err(why) => panic!("Couldn't convert to BSON: {}", why),
-            Ok(d)    => {
-                if let ElectionDatabase::Dail(ref client, ref db) = *self {
-                    let coll = client.db(db).collection("area");
+    fn create_document<'a,T>(&self, areas: &'a Vec<T>) where T: serde::Serialize {
+        println!("[mongoloid] Creating database \"{}\"...", self.get_database());
+        let docs: Vec<OrderedDocument> = areas.iter().filter_map(|a| {
+            match bson::to_bson(a) {
+                Err(why) => {
+                        eprintln!("[mongoloid] Couldn't convert row to BSON: {}", why);
+                        None
+                    },
+                Ok(d) => {
                     if let bson::Bson::Document(bson_doc) = d {
-                        coll.insert_one(bson_doc, None)
-                            .ok().expect("Failed to insert document.");
+                        return Some(bson_doc);
                     }
-                }
+                    None
+                },
             }
+        }).collect();
+        if let ElectionDatabase::Dail(ref client, ref db) = *self {
+            let coll = client.db(db).collection("area");
+            
+            coll.insert_many(docs, None)
+                .ok().expect("Failed to insert document.");
         }
     }
 }
